2a3688a91103e8828c544908f218824ff1fbc293
John Hoff <john.hoff@braindonor.net>
2019-03-08 21:39:15 -0600

    Working through final model creation

--- model/model_scoring_search.py
+++ model/model_scoring_search.py
@@ -21,7 +21,7 @@ from utility import find_optimal_f1_threshold, Logger, use_project_path
 
 input_predictions = [
     'neural_network_basic',
-    #'neural_network_basic_fs',
+    'neural_network_basic_fs',
     'sgd_huber_loss_over_sampled',
     'sgd_huber_loss_over_sampled_fs',
     'xgboost_basic',
